1bc3615e6668869af2afa101b9ab381dbdaa3462
Guthib-of-Dan <diril656@gmail.com>
2026-04-25 17:48:27 +0000

    Last step before test release

--- src/installer.js
+++ src/installer.js
@@ -1,18 +1,18 @@
 // https://github.com/<owner>/<repo>/releases/download/<tag>/<filename>
-var fs = require("node:fs/promises")
 var repo = "Guthib-of-Dan/uWebSockets.js"
-var tag = "v20.66.0"
-export default async function downloadBinary(paramTag = tag, paramFilename = 'uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node') {
-  var link = "https://github.com/" + repo + "/releases/download/" + paramTag + "/";
+var tag = "v0.66.02"
+
+module.exports = async function downloadBinary(paramTag = tag, paramFilename = 'uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node') {
+  var execSync = require("node:child_process").execSync
+  var archiveName = paramFilename + ".tar.gz";
+  var link = "https://github.com/" + repo + "/releases/download/" + paramTag + "/" + archiveName;
 
   console.log("Expected binary - " + paramFilename);
+  execSync(`curl -sS '${link}' -o ${archiveName} && tar -xzf ${paramFilename} ${archiveName} && rm ${archiveName}`, {stdio: "inherit", encoding: "utf16le"}, ); 
 
-  var res = await fetch(link + paramFilename)
-  var buffer = await res.arrayBuffer()
-  await fs.writeFile(paramFilename, buffer)
   console.log("finished")
 }
 
 if (process.argv[2] == "default") {
-  downloadBinary();
+  module.exports();
 }
--- src/uws.js
+++ src/uws.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const VERSION = "v20.66.0"
+const VERSION = "v0.66.02"
 
 module.exports = (() => {
 	try {

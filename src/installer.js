42ee4eda5ecd298aad025e78d47eefedb60315f5
Guthib-of-Dan <diril656@gmail.com>
2026-04-25 12:00:10 +0000

    Fixed ASAN CI + updated installer

--- src/installer.js
+++ src/installer.js
@@ -1,10 +1,18 @@
 // https://github.com/<owner>/<repo>/releases/download/<tag>/<filename>
 var fs = require("node:fs/promises")
 var repo = "Guthib-of-Dan/uWebSockets.js"
-var link = "https://github.com/" + repo + "/releases/download/" +
-  /*TAG*/ "v0.0.0"
-  + "/";
-var requiredFilename = 'uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node'
-fetch(link + requiredFilename)
-  .then((res)=>res.arrayBuffer())
-  .then((buffer)=>fs.writeFile(requiredFilename, buffer))
+var tag = "v0.0.0"
+export default async function downloadBinary(paramTag = tag, paramFilename = 'uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node') {
+  var link = "https://github.com/" + repo + "/releases/download/" + paramTag + "/";
+
+  console.log("Expected binary - " + paramFilename);
+
+  var res = await fetch(link + paramFilename)
+  var buffer = await res.arrayBuffer()
+  await fs.writeFile(paramFilename, buffer)
+  console.log("finished")
+}
+
+if (process.argv[2] == "default") {
+  downloadBinary();
+}
